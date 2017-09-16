7b2247150d750e1787e7a0b89d714c82ce77b400
Alex Di Battista <alex.dibattista@gmail.com>
2017-09-15 21:02:12 -0400

    open modal and parse HTML for player name

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /*
  * @flow
  */
+ const puppeteer = require('puppeteer');
+ const cliSpinners = require('cli-spinners');
 
 const credentials = require('./credentials');
-const puppeteer = require('puppeteer');
+
 
 // HTML Selectors
 const USERNAME_SELECTOR = '#login-username';
@@ -13,28 +15,35 @@ const PASSWORD_SUBMIT_SELECTOR = '#login-signin';
 const LENGTH_OF_TABLE = 'tr';
 
 let LIST_PLAYER_SELECTOR = '.Table > tbody > tr:nth-child(INDEX) > .player > div > .Grid-bind-end > .ysf-player-name > .name';
+let PLAYER_STATUS_SELECTOR = '.Table > tbody > tr:nth-child(INDEX) > .player > div > .Grid-bind-end > .player-status > a';
 
 async function run() {
   const yahooLogin = 'https://login.yahoo.com';
   const yahooFootball = 'https://football.fantasysports.yahoo.com/';
   const yahooFootballAllPlayers = 'https://football.fantasysports.yahoo.com/f1/38877/players?&sort=AR&sdir=1&status=ALL&pos=O&stat1=S_S_2017&jsenabled=1';
   const browser = await puppeteer.launch({
-    headless: true // debugging
+    headless: false, // false for debugging
+    timeout: 0
   });
   const page = await browser.newPage();
   await page.goto(yahooLogin);
 
   // Click on the login input box
+  console.log('Enter Username');
   page.click(USERNAME_SELECTOR);
-  page.type(credentials.username);
+  page.type(credentials.username)
+  console.log('Submit Username');
   page.click(USERNAME_SUBMIT_SELECTOR);
   await page.waitForNavigation();
 
   // Click on the login input box
+  console.log('Enter Password');
   page.click(PASSWORD_SELECTOR);
-  page.type(credentials.password);
+  page.type(credentials.password)
+  console.log('Submit Password');;
   page.click(PASSWORD_SUBMIT_SELECTOR);
   await page.waitForNavigation();
+  console.log('Go to yahoo football');
   await page.goto(yahooFootballAllPlayers);
 
   let tableLength = await page.evaluate((sel) => {
@@ -44,13 +53,14 @@ async function run() {
 
   console.log(`number of rows: ${tableLength}`);
 
-
   for (let i = 1; i <= tableLength; i++) {
-    let playerNameSelector = LIST_PLAYER_SELECTOR.replace("INDEX", i);
+    let playerNameSelector = PLAYER_STATUS_SELECTOR.replace("INDEX", i);
+    page.click(playerNameSelector);
+    await page.waitFor(2 * 1000);
 
-    let playerName = await page.evaluate((sel) => {
-      return document.querySelector(sel).innerHTML;
-    }, playerNameSelector);
+    let playerName = await page.evaluate(() => {
+      return document.querySelector('.yui3-widget-bd > div > div > .dynamicnote-hd > .playerinfo > .hd > h4 > .name').innerHTML;
+    });
 
     console.log(playerName);
   }
@@ -58,8 +68,8 @@ async function run() {
 
 
 
-  browser.close();
-
+  // browser.close();
+  // scrape defenses
   // Naviagate to players information
   // loop through all the players and save data to CSV.
 
