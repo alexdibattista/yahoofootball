10e91739bef3cf9471cc64cb1cf5732f87b11c3e
Alexander Di Battista <alex.dibattista@gmail.com>
2017-09-17 00:17:28 -0400

    worked on getting cookies going(didn't go well) started grabbing the weekly stats for players

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @flow
  */
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const credentials = require('./credentials');
 
@@ -24,25 +25,56 @@ async function run() {
     headless: true, // false for debugging
     timeout: 0
   });
+
   const page = await browser.newPage();
   await page.goto(yahooLogin);
 
-  // Click on the login input box
-  console.log('Enter Username');
-  page.click(USERNAME_SELECTOR);
-  page.type(credentials.username)
-  console.log('Submit Username');
-  page.click(USERNAME_SUBMIT_SELECTOR);
-  await page.waitForNavigation();
+  // if (fs.existsSync('cookies.json')) {
+  //   let cookies = fs.readFile('cookies.json', 'utf8', function readFileCallback(err, data) {
+  //     if (err) {
+  //       console.log(`error: ${err}`);
+  //     }
+  //     else {
+  //       page.setCookie(JSON.parse(data));
+  //       return JSON.parse(data); //now it an object
+  //     }
+  //   });
+  //   console.log(`cookies: ${cookies}`);
+  //
+  //
+  // }
+  // else {
+    // Click on the login input box
+    console.log('Enter Username');
+    page.click(USERNAME_SELECTOR);
+    page.type(credentials.username)
+    console.log('Submit Username');
+    page.click(USERNAME_SUBMIT_SELECTOR);
+    await page.waitForNavigation();
 
-  // Click on the login input box
-  console.log('Enter Password');
-  page.click(PASSWORD_SELECTOR);
-  page.type(credentials.password)
-  console.log('Submit Password');;
-  page.click(PASSWORD_SUBMIT_SELECTOR);
-  await page.waitForNavigation();
+
+    // Click on the login input box
+    console.log('Enter Password');
+    page.click(PASSWORD_SELECTOR);
+    page.type(credentials.password)
+    console.log('Submit Password');;
+    page.click(PASSWORD_SUBMIT_SELECTOR);
+    await page.waitForNavigation();
+  // }
+
+
+
   console.log('Go to yahoo football');
+
+  // console.log('Write cookies to file');
+  // const cookies = await page.cookies();
+  // let json = JSON.stringify(cookies);
+  // fs.writeFile('cookies.json', json, 'utf8', () => {
+  //   console.log('wrote to file');
+  // });
+
+
+
   await page.goto(yahooFootballAllPlayers);
 
   let tableLength = await page.evaluate((sel) => {
@@ -57,7 +89,7 @@ async function run() {
   for (let i = 1; i <= tableLength; i++) {
     let playerNameSelector = PLAYER_STATUS_SELECTOR.replace("INDEX", i);
     page.click(playerNameSelector);
-    await page.waitFor(2 * 1000);
+    await page.waitFor(5 * 1000);
 
     let player = await page.evaluate((sel) => {
 
@@ -66,12 +98,18 @@ async function run() {
       let position = document.querySelector('.yui3-widget-bd > div > div > .dynamicnote-hd > .playerinfo > .playerdetails > dl > dd.pos').getElementsByTagName('span')[0].remove();
       position = document.querySelector('.yui3-widget-bd > div > div > .dynamicnote-hd > .playerinfo > .playerdetails > dl > dd.pos').innerHTML;
       let team = document.querySelector('.yui3-widget-bd > div > div > .dynamicnote-hd > .playerinfo > .playerdetails > dl > dd.team.last > a').innerHTML;
+      let statsType = document.querySelector('.yui3-widget-bd > div > div > .yui3-ysplayernote-tabview > .dynamicnote-bd > .playernoteview-notes > .playerdata > .teamsched > .teamsched-bd > h5').innerHTML;
+      let STATS_HEADER_COLUMNS = document.querySelector('.yui3-widget-bd > div > div > .yui3-ysplayernote-tabview > .dynamicnote-bd > .playernoteview-notes > .playerdata > .teamsched > .teamsched-bd > .teamtable > thead > tr');
+      let STATS_HEADER_COLUMNS_LENGTH = STATS_HEADER_COLUMNS.length;
+      console.log(STATS_HEADER_COLUMNS);
+      console.log(STATS_HEADER_COLUMNS_LENGTH);
 
       let player = {
         name,
         number,
         position,
-        team
+        team,
+        statsType
       };
 
       return player;
